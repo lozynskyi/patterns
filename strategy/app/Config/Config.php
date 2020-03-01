5e7ee53ca6cd131d3f3fa1e8abd378d8529af7fa
Slayter <slayter007@gmail.com>
2020-03-01 17:12:04 +0200

    Add contractors and parsers

--- strategy/app/Config/Config.php
+++ strategy/app/Config/Config.php
@@ -2,7 +2,44 @@
 
 namespace App\Config;
 
+use App\Config\Contracts\ParserInterface;
+
 class Config
 {
+    private $parser;
 
+    protected $config;
+
+    public function __construct(ParserInterface $parser)
+    {
+        $this->setParser($parser);
+    }
+
+    public function setParser(ParserInterface $parser)
+    {
+        $this->parser = $parser;
+    }
+
+    public function load($file)
+    {
+        $this->config = $this->parser->parse($file);
+    }
+
+    public function get($key, $default = null)
+    {
+        $keys = explode('.', $key);
+        $config = $this->config;
+
+        foreach ($keys as $key) {
+            if (isset($config[$key])) {
+                $config = $config[$key];
+                continue;
+            } else {
+                $config = $default;
+                break;
+            }
+        }
+
+        return $config;
+    }
 }
--- strategy/index.php
+++ strategy/index.php
@@ -2,4 +2,10 @@
 
 require 'vendor/autoload.php';
 
-$config = new \App\Config\Config();
+$config = new \App\Config\Config(
+    new \App\Config\Parser\ArrayParser()
+);
+
+$config->load('config/database.php');
+
+var_dump($config->get('mysql.host'));
