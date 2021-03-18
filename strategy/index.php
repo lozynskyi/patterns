279e935b9f83d1a955787d68dff4e219ab0e6b44
Pavlo Lozynskyi <lozynskyi@admins-MacBook-Pro-2.local>
2021-03-18 12:02:03 +0200

    Add examples for strategy pattern

--- strategy/index.php
+++ strategy/index.php
@@ -2,10 +2,16 @@
 
 require 'vendor/autoload.php';
 
+//Array Parser
 $config = new \App\Config\Config(
     new \App\Config\Parser\ArrayParser()
 );
-
 $config->load('config/database.php');
-
 var_dump($config->get('mysql.host'));
+
+//Json Parser
+$config = new \App\Config\Config(
+    new \App\Config\Parser\JsonParser()
+);
+$config->load('config/database.json');
+var_dump($config->get('mysql.port'));
