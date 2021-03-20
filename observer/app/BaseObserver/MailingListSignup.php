3d99f40587f3ea66f5244bb9158770b9098861d0
Slayter <slayter007@gmail.com>
2021-03-20 18:18:46 +0200

    Move event realisation to trait

--- observer/app/BaseObserver/MailingListSignup.php
+++ observer/app/BaseObserver/MailingListSignup.php
@@ -4,38 +4,16 @@ namespace App\BaseObserver;
 
 use App\BaseObserver\Contracts\Observer;
 use App\BaseObserver\Contracts\Subject;
+use App\BaseObserver\Traits\Subjectable;
 
 class MailingListSignup implements Subject
 {
-    public $user;
+    use Subjectable;
 
-    /** @var Observer[] */
-    protected $observers = [];
+    public $user;
 
     public function __construct($user)
     {
         $this->user = $user;
-    }
-
-    public function attach(Observer $observer)
-    {
-        $this->observers[] = $observer;
-    }
-
-    public function detach(Observer $observer)
-    {
-        for ($i = 0; $i < count($this->observers); $i++) {
-            if ($this->observers[$i] === $observer) {
-                unset($this->observers[$i]);
-            }
-        }
-        $this->observers = array_values($this->observers);
-    }
-
-    public function notify()
-    {
-        foreach ($this->observers as $observer) {
-            $observer->handle($this);
-        }
     }
 }
---
+++ observer/app/BaseObserver/Traits/Subjectable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\BaseObserver\Traits;
+
+use App\BaseObserver\Contracts\Observer;
+
+trait Subjectable
+{
+    /** @var Observer[] */
+    protected $observers = [];
+
+    public function attach(Observer $observer)
+    {
+        $this->observers[] = $observer;
+    }
+
+    public function detach(Observer $observer)
+    {
+        for ($i = 0; $i < count($this->observers); $i++) {
+            if ($this->observers[$i] === $observer) {
+                unset($this->observers[$i]);
+            }
+        }
+        $this->observers = array_values($this->observers);
+    }
+
+    public function notify()
+    {
+        foreach ($this->observers as $observer) {
+            $observer->handle($this);
+        }
+    }
+}
