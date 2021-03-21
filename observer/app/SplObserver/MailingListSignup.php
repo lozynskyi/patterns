d300140b16aec4806d2bd07a3ad449019974e4c7
Slayter <slayter007@gmail.com>
2021-03-21 22:34:14 +0200

    Add Spl implementation

---
+++ observer/app/SplObserver/MailingListSignup.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\SplObserver;
+
+use SplObserver;
+
+class MailingListSignup implements \SplSubject
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function attach(SplObserver $observer)
+    {
+        // TODO: Implement attach() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function detach(SplObserver $observer)
+    {
+        // TODO: Implement detach() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function notify()
+    {
+        // TODO: Implement notify() method.
+    }
+}
---
+++ observer/app/SplObserver/UpdateMailingStatusInDatabase.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\SplObserver;
+
+use SplSubject;
+
+class UpdateMailingStatusInDatabase implements \SplObserver
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function update(SplSubject $subject)
+    {
+        // TODO: Implement update() method.
+    }
+}
