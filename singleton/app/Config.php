67c3d8b2ed3a1a880a8d14a90a3efbd1c3d18604
Slayter <slayter007@gmail.com>
2021-03-23 21:47:09 +0200

    Add Singleton check

---
+++ singleton/app/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class Config extends Singleton
+{
+    public $data = [
+        'db' => [
+            'host' => '127.0.0.1'
+        ]
+    ];
+}
--- singleton/index.php
+++ singleton/index.php
@@ -1,3 +1,10 @@
 <?php
 
+use App\Config;
+
 require 'vendor/autoload.php';
+
+$config = Config::getInstance();
+$anotherConfig = Config::getInstance();
+
+var_dump($config === $anotherConfig);
