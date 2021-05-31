346fb2d52ca1af6f6f09403c967ab202562b82a8
Pavlo <slayter007@gmail.com>
2021-05-31 23:30:21 +0300

    Update ExampleTest.php

--- event_dispatcher/tests/Unit/ExampleTest.php
+++ event_dispatcher/tests/Unit/ExampleTest.php
@@ -4,11 +4,11 @@ namespace App\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class EventTest extends TestCase
 {
   
   /** @test */
-  public function example()
+  public function can_get_event_name()
   {
     $this->assertTrue(true);
   }
