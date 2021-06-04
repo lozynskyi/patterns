5b45f17bc1f6de8dbe3314f6fffd58ea7aba6be3
Pavlo <slayter007@gmail.com>
2021-06-04 12:22:36 +0300

    Update LegalHandler.php

--- chain_of_responsibilities/app/LegalHandler.php
+++ chain_of_responsibilities/app/LegalHandler.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace App;
+
+use Transaction;
+
+class LegalHandler extends AbstractHandler
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function process(Transaction $transaction)
+    {
+        if (!$transaction->legal) {
+            echo 'Legal -- scipped!\n';
+        }
+         echo 'Legal -- processed!\n';
+        
+        $this->next($transaction);
+    }
+}
