452fce98f6f1f41902bb9e39361fa3154cf02005
Pavlo <slayter007@gmail.com>
2021-06-04 12:30:18 +0300

    Update TaxHandler.php

--- chain_of_responsibilities/app/TaxHandler.php
+++ chain_of_responsibilities/app/TaxHandler.php
@@ -9,10 +9,10 @@ class TaxHandler extends AbstractHandler
      */
     public function process(Transaction $transaction)
     {
-        if (!$transaction->fiscal) {
-            echo 'Fiscal -- scipped!\n';
+        if (!$transaction->tax) {
+            echo 'Tax -- scipped!\n';
         }
-         echo 'Fiscal -- processed!\n';
+         echo 'Tax -- processed!\n';
         
         $this->next($transaction);
     }
