25581c2e72d110a280e9004eec41b6a72bf9c236
Pavlo <slayter007@gmail.com>
2021-06-04 11:57:37 +0300

    Update AbstractHandler.php

--- chain_of_responsibilities/app/AbstractHandler.php
+++ chain_of_responsibilities/app/AbstractHandler.php
@@ -1,1 +1,35 @@
+<?php
+
+namespace app;
+
+use Transaction;
+
+abstract class AbstractHandler
+{
+    /**
+     * @var AbstractHandler
+     */
+    protected AbstractHandler $handler;
+
+    /**
+     * @param PspTransaction $transaction
+     * @return mixed
+     */
+    abstract public function process(Transaction $transaction);
+
+    /**
+     * @param AbstractHandler $handler
+     */
+    public function setHandler(AbstractHandler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    public function next(Transaction $transaction)
+    {
+        if ($this->handler) {
+            $this->handler->process($transaction);
+        }
+    }
+}
 
