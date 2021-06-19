99f74b13ae88eff3313f615bd26b84825575a057
Slayter <slayter007@gmail.com>
2021-06-19 23:53:52 +0300

    Add listeners

--- event_dispatcher/app/Core/Events/Dispatcher.php
+++ event_dispatcher/app/Core/Events/Dispatcher.php
@@ -10,4 +10,8 @@ class Dispatcher
     {
         return $this->listeners;
     }
+    public function addListener($name, Listener $listener)
+    {
+        $this->listeners[$name][] = $listener;
+    }
 }
--- event_dispatcher/tests/Unit/DispatcherTest.php
+++ event_dispatcher/tests/Unit/DispatcherTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Unit;
 use App\Core\Events\Dispatcher;
 use App\Tests\Stubs\EventStub;
 use App\Tests\Stubs\EventStubNoName;
+use App\Tests\Stubs\ListenerStub;
 use PHPUnit\Framework\TestCase;
 
 class DispatcherTest extends TestCase
@@ -17,4 +18,14 @@ class DispatcherTest extends TestCase
       $this->assertEmpty($dispatcher->getListeners());
       $this->assertIsArray($dispatcher->getListeners());
   }
+
+    /** @test */
+    public function it_can_add_listener()
+    {
+        $dispatcher = new Dispatcher();
+        $dispatcher->addListener('UserSignUp', new ListenerStub());
+
+        $this->assertCount(1, $dispatcher->getListeners());
+        $this->assertIsArray($dispatcher->getListeners()['UserSignUp']);
+    }
 }
