30b93cd0fd0b43836becbee621ac3910cda3a117
Slayter <slayter007@gmail.com>
2021-06-20 22:53:24 +0300

    Add new test

--- event_dispatcher/tests/Unit/DispatcherTest.php
+++ event_dispatcher/tests/Unit/DispatcherTest.php
@@ -11,13 +11,13 @@ use PHPUnit\Framework\TestCase;
 class DispatcherTest extends TestCase
 {
 
-  /** @test */
-  public function it_holds_listeners_in_array()
-  {
+    /** @test */
+    public function it_holds_listeners_in_array()
+    {
       $dispatcher = new Dispatcher();
       $this->assertEmpty($dispatcher->getListeners());
       $this->assertIsArray($dispatcher->getListeners());
-  }
+    }
 
     /** @test */
     public function it_can_add_listener()
@@ -27,5 +27,14 @@ class DispatcherTest extends TestCase
 
         $this->assertCount(1, $dispatcher->getListeners());
         $this->assertIsArray($dispatcher->getListeners()['UserSignUp']);
+    }
+
+    /** @test */
+    public function it_can_get_listener_by_event_name()
+    {
+        $dispatcher = new Dispatcher();
+        $dispatcher->addListener('UserSignUp', new ListenerStub());
+
+        $this->assertCount(1, $dispatcher->getListenersByEvent('UserSignUp'));
     }
 }
