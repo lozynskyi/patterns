6af826784cd078aeddcfeb91589c86bd654eefa3
Slayter <slayter007@gmail.com>
2021-06-20 23:42:13 +0300

    Add stub

--- event_dispatcher/tests/Integration/IntegrationTest.php
+++ event_dispatcher/tests/Integration/IntegrationTest.php
@@ -2,15 +2,21 @@
 
 namespace App\Tests\Integration;
 
+use App\Core\Events\Dispatcher;
+use App\Tests\Stubs\EventStub;
+use App\Tests\Stubs\ListenerStub;
 use PHPUnit\Framework\TestCase;
 
 class IntegrationTest extends TestCase
 {
-  
-  /** @test */
-  public function can_get_event_name()
-  {
-    $this->assertTrue(true);
-  }
-  
+
+    /** @test */
+    public function can_get_event_name()
+    {
+        $dispatcher = new Dispatcher();
+
+        $event = new EventStub();
+        $mockerListener = $this->createMock(ListenerStub::class);
+    }
+
 }
