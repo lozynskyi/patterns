8fee0ba074c8182f742ea4ea1d66efd7467f2e7f
Slayter <slayter007@gmail.com>
2021-06-21 22:57:55 +0300

    Fix execution

--- event_dispatcher/index.php
+++ event_dispatcher/index.php
@@ -2,13 +2,20 @@
 
 use App\Core\Events\Dispatcher;
 use App\Models\User;
+use \App\Listeners\User\UpdateSignInDate;
+use \App\Listeners\User\SendSignInEmail;
+use \App\Events\User\UserSignedIn;
 
 require_once 'vendor/autoload.php';
-
-echo ('event dispatch start');
 
 $user = new User();
 $user->id = 1;
 $user->email = 'some@email.com';
 
-$dispatcher = new Dispatcher();
+$dispatcher = new Dispatcher;
+
+$dispatcher->addListener('UserSignedIn', new UpdateSignInDate());
+
+$dispatcher->addListener('UserSignedIn', new SendSignInEmail());
+
+$dispatcher->dispatch(new UserSignedIn($user));
