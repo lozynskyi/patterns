ea4db46bd07d4b9c9417b05f03a28fec75e1070b
Slayter <slayter007@gmail.com>
2021-06-21 22:39:55 +0300

    Update init index

--- event_dispatcher/index.php
+++ event_dispatcher/index.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Core\Events\Dispatcher;
+use App\Models\User;
+
 require_once 'vendor/autoload.php';
 
 echo ('event dispatch start');
+
+$user = new User();
+$user->id = 1;
+$user->email = 'some@email.com';
+
+$dispatcher = new Dispatcher();
