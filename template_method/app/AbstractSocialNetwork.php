751bc485d3ac86d710415fee31b0b5f91e04b7ee
Pavlo <slayter007@gmail.com>
2021-12-04 17:57:02 +0200

    Update and rename SocialNetwork.php to AbstractSocialNetwork.php

---
+++ template_method/app/AbstractSocialNetwork.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+abstract class AbstractSocialNetwork
+{
+    protected $username;
+
+    protected $password;
+
+    public function __construct(string $username, string $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+}
