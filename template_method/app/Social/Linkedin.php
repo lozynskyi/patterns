8cd1b0708eb965794e13cd4e08618fb502e1051b
Pavlo <slayter007@gmail.com>
2021-12-04 18:01:51 +0200

    Update Linkedin.php

--- template_method/app/Social/Linkedin.php
+++ template_method/app/Social/Linkedin.php
@@ -1,1 +1,30 @@
 <?php
+
+use App\AbstractSocialNetwork;
+
+class Linkedin extends AbstractSocialNetwork
+{
+    public function logIn(string $userName, string $password): bool
+    {
+        echo "\nChecking user's credentials...\n";
+        echo "Name: " . $this->username . "\n";
+        echo "Password: " . str_repeat("*", strlen($this->password)) . "\n";
+
+        echo "\n\nLinkedin: '" . $this->username . "' has logged in successfully.\n";
+
+        return true;
+    }
+
+    public function sendData(string $message): bool
+    {
+        //Concrete realisation for send data.
+        echo "Linkedin: '" . $this->username . "' has posted '" . $message . "'.\n";
+
+        return true;
+    }
+
+    public function logOut(): void
+    {
+        echo "Linkedin: '" . $this->username . "' has been logged out.\n";
+    }
+}
