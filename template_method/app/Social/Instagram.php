72feb2afad9a9576feeb5746138dfa78c0ede36c
Pavlo <slayter007@gmail.com>
2021-12-04 18:08:25 +0200

    Update Instagram.php

--- template_method/app/Social/Instagram.php
+++ template_method/app/Social/Instagram.php
@@ -6,5 +6,27 @@ use App\AbstractSocialNetwork;
 
 class Instagram extends AbstractSocialNetwork
 {
+    public function logIn(string $userName, string $password): bool
+    {
+        echo "\nChecking user's credentials...\n";
+        echo "Name: " . $this->username . "\n";
+        echo "Password: " . str_repeat("*", strlen($this->password)) . "\n";
 
+        echo "\n\nInstagram: '" . $this->username . "' has logged in successfully.\n";
+
+        return true;
+    }
+
+    public function sendData(string $message): bool
+    {
+        //Concrete realisation for send data.
+        echo "Instagram: '" . $this->username . "' has posted '" . $message . "'.\n";
+
+        return true;
+    }
+
+    public function logOut(): void
+    {
+        echo "Instagram: '" . $this->username . "' has been logged out.\n";
+    }
 }
