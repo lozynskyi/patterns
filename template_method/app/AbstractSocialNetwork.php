80f1eb69552de64726b4a708ccf87ece094182af
Pavlo <slayter007@gmail.com>
2021-12-04 17:59:03 +0200

    Update AbstractSocialNetwork.php

--- template_method/app/AbstractSocialNetwork.php
+++ template_method/app/AbstractSocialNetwork.php
@@ -13,4 +13,23 @@ abstract class AbstractSocialNetwork
         $this->username = $username;
         $this->password = $password;
     }
+    
+    public function post(string $message): bool
+    {
+        // Authenticate before posting.
+        if ($this->logIn($this->username, $this->password)) {
+            $result = $this->sendData($message);
+            $this->logOut();
+
+            return $result;
+        }
+
+        return false;
+    }
+    
+    abstract public function logIn(string $userName, string $password): bool;
+
+    abstract public function sendData(string $message): bool;
+
+    abstract public function logOut(): void;
 }
