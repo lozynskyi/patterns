f007569c5169a7f096107f682b28e74494df2b6a
Pavlo <slayter007@gmail.com>
2021-12-06 23:54:28 +0200

    Update index.php

--- template_method/index.php
+++ template_method/index.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Social\Instagram;
+use App\Social\Linkedin;
+
 require 'vendor/autoload.php';
 
 var_dump('Start template method pattern.');
 
 
-$linkedin = new App\Social\Linkedin();
+$linkedin = new Linkedin();
 $linkedin->logIn('username', 'password');
+
+$instagram = new Instagram();
+$instagram->logIn('username', 'password');
