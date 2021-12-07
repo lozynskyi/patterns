943a4018aa5eb6d8e6d9d5fbd03b01c0831e35e1
Pavlo <slayter007@gmail.com>
2021-12-07 23:39:02 +0200

    Update InventoryList.php

--- iterator/app/InventoryList.php
+++ iterator/app/InventoryList.php
@@ -1,1 +1,24 @@
+<?php
 
+declare(strict_types=1);
+
+namespace App;
+
+use Countable;
+use Iterator;
+
+class InventoryList implements Countable, Iterator
+{
+    /**
+     * @var Inventory[]
+     */
+    private array $inventory = [];
+    private int $currentIndex = 0;
+
+    public function addInventory(Inventory $inventory)
+    {
+        $this->inventory[] = $inventory;
+    }
+
+
+}
