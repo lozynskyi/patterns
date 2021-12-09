0eaf7959f9066b50dc69ffd30c0b80fade0cc4c7
Pavlo <slayter007@gmail.com>
2021-12-09 23:21:07 +0200

    Update Inventory.php

--- iterator/app/Inventory.php
+++ iterator/app/Inventory.php
@@ -4,5 +4,23 @@ class Inventory {
 
     private $author;
     private $title;
+      
+    public function __construct($title, $author)
+    {
+      $this->author = $author;
+      $this->title  = $title;
+    }
   
+    public function getAuthor() {
+      return $this->author;
+    }
+ 
+    public function getTitle()
+    {
+      return $this->title;
+    }
+  
+    public function getAuthorAndTitle() {
+      return $this->getTitle() . ' by ' . $this->getAuthor();
+    }
 }
