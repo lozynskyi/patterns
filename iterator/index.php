ca7cd8674898c643d55b47a8c2f7d02e4b516215
Pavlo <slayter007@gmail.com>
2021-12-11 23:28:25 +0200

    Update index.php

--- iterator/index.php
+++ iterator/index.php
@@ -12,3 +12,9 @@ $inventoryList = new InventoryList();
 $inventoryList->add(new Inventory('Document one', 'Bob Dylan'));
 $inventoryList->add(new Inventory('Document two', 'John Dow'));
 $inventoryList->add(new Inventory('Document thre', 'Olvap Iyksnyzol'));
+
+$inventorys = [];
+
+foreach ($inventoryList as $inventory) {
+    $inventorys[] = $book->getAuthorAndTitle();
+}
