9841ccabbb646a61630bc30c08fe3e888d833f8a
Pavlo <slayter007@gmail.com>
2021-12-16 23:48:00 +0200

    Update Service.php

--- null_object/app/Service.php
+++ null_object/app/Service.php
@@ -1,1 +1,19 @@
+<?php
 
+declare(strict_types=1);
+
+namespace App;
+
+class Service
+{
+    
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    public function action()
+    {
+        // notice here that you don't have to check if the logger is set with eg. is_null(), instead just use it
+        $this->logger->log('We are in ' . __METHOD__);
+    }
+}
