d20e015114f31a4d78688d04ed49939d843cdfdd
Pavlo <slayter007@gmail.com>
2021-12-17 14:18:42 +0200

    Update index.php

--- null_object/index.php
+++ null_object/index.php
@@ -7,3 +7,7 @@ require 'vendor/autoload.php';
 
 $service = new Service(new NullLogger());
 $service->action();
+
+$service = new Service(new MimicLogger());
+$this->action('We are in App\Service::action');
+$service->action();
