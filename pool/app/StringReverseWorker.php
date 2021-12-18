ca6f5629e86d9675b1cb12dcd0a3826651f134df
Pavlo <slayter007@gmail.com>
2021-12-18 23:47:44 +0200

    Update StringReverseWorker.php

--- pool/app/StringReverseWorker.php
+++ pool/app/StringReverseWorker.php
@@ -1,1 +1,19 @@
+<?php
 
+declare(strict_types=1);
+
+namespace App;
+
+use DateTime;
+
+class StringReverseWorker
+{
+    public function __construct()
+    {
+    }
+
+    public function run(string $text): string
+    {
+        return strrev($text);
+    }
+}
