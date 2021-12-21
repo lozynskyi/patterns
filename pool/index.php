2a07a961ab5ed63edb99f2c1a78a56082d756668
Pavlo <slayter007@gmail.com>
2021-12-21 11:51:49 +0200

    Update index.php

--- pool/index.php
+++ pool/index.php
@@ -1,1 +1,9 @@
+use App\WorkerPool;
 
+require 'vendor/autoload.php';
+
+$pool = new WorkerPool();
+$worker1 = $pool->get();
+$worker2 = $pool->get();
+
+var_dump('Workers pool contain: ' . count($pool));
