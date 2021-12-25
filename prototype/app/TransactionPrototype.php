23818692f652aa79cb5a38256230b45089a26e06
Pavlo <slayter007@gmail.com>
2021-12-25 23:32:59 +0200

    Update TransactionPrototype.php

--- prototype/app/TransactionPrototype.php
+++ prototype/app/TransactionPrototype.php
@@ -1,1 +1,33 @@
+<?php
 
+namespace App;
+
+
+abstract class TransactionPrototype
+{
+    protected string $description;
+    protected string $topic;
+    protected float $amount;
+  
+    abstract function __clone();
+    
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    
+    public function getTopic()
+    {
+        return $this->topic;
+    }
+  
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
