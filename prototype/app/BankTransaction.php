0ad877e177a40203bf90121f7a9d72cfa6f092a1
Pavlo <slayter007@gmail.com>
2021-12-25 23:51:53 +0200

    Update BankTransaction.php

--- prototype/app/BankTransaction.php
+++ prototype/app/BankTransaction.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace App;
+
+class BankTransaction extends TransactionPrototype
+{
+    public function __construct($amount, $description) {
+        $this->topic = 'Bank';
+        $this->amount = $amount;
+        $this->description = $description;
+    }
+    
+    public function __clone()
+    {
+    }
+}
