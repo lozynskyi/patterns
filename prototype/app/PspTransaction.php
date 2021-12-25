b3771d94cf658106e469bb85ed5589e8c2f9aef2
Pavlo <slayter007@gmail.com>
2021-12-25 23:47:47 +0200

    Update PspTransaction.php

--- prototype/app/PspTransaction.php
+++ prototype/app/PspTransaction.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace App;
+
+class PspTransaction extends TransactionPrototype
+{
+    function __construct($amount, $description) {
+        $this->topic = 'PSP';
+        $this->amount = $amount;
+        $this->description = $description;
+    }
+}
