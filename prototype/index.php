cabafdcf750ee47f29752139d194949203b0f140
Pavlo <slayter007@gmail.com>
2021-12-27 23:49:27 +0200

    Update index.php

--- prototype/index.php
+++ prototype/index.php
@@ -7,3 +7,11 @@ require 'vendor/autoload.php';
 
 var_dump('Prototype Pattern');
 
+$pspProto = new PspTransaction();
+$bankProto = new BankTransaction();
+
+$transaction1 = clone $pspProto;
+$transaction1->setDescription('Psp Description from order');
+echo('Transaction 1 topic: '.$transaction1->getTopic());
+echo('Transaction 1 description: '.$transaction1->getDescription());
+echo('\n');
